d3a6f66ac9ba2e9d551adffe92e4ed8799514878
Maxime RIO <maxime.rio@niwa.co.nz>
2021-02-03 10:50:45 +0000

    update code to remove warnings and ensure it runs on NeSI

--- notebooks/fig_code/sgd_separator.py
+++ notebooks/fig_code/sgd_separator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import SGDClassifier
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import make_blobs
 
 def plot_sgd_separator():
     # we create 50 separable points
